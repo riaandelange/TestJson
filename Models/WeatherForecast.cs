3dfab0c485334612b48f38b0596bcb5352b6db1e
Riaan de Lange <riaan@delango.co.za>
2024-11-27 19:22:53 +0200

    Add project files.

---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TestJson.Models
+{
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+    }
+}
